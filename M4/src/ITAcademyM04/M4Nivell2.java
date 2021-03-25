ef3384c3bc5c0c4aa5b69122c7777c4f64c5c1d7
Victor Cuadrat <46792356+victorcuadrat@users.noreply.github.com>
2021-03-25 22:39:02 +0100

    classes 4 deviced

--- M4/src/ITAcademyM04/M4Nivell2.java
+++ M4/src/ITAcademyM04/M4Nivell2.java
@@ -1,10 +1,31 @@
 package ITAcademyM04;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+import com.sun.tools.javac.util.ArrayUtils;
+import com.sun.tools.javac.util.List;
+
 public class M4Nivell2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/**
+		 * NIVELL 2
+		 * Exercici: Revisaràs l'exercici anterior modificant-lo per afegir 
+		 * noves excepcions més adients. 
+		 * 
+		 * Modifica la revisió de la comanda, si un usuari introdueix un plat 
+		 * que no existeix, inmediatament executa una excepció per avisar que el producte
+		 * no existeix y no l’afageixis a la llista.  
+		 * Crea una Excepció per la pregunta Si/No al seguir demanant e implementala.
+		 */
 	}
 
 }
--- M4/src/ITAcademyM04/M4Nivell3.java
+++ M4/src/ITAcademyM04/M4Nivell3.java
@@ -1,5 +1,33 @@
 package ITAcademyM04;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+import com.sun.tools.javac.util.ArrayUtils;
+import com.sun.tools.javac.util.List;
+
 public class M4Nivell3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		
+		/** 
+		 * NIVELL 3
+		 * Exercici: Crea excepcions personalitzades amb fitxers nous e implementa-les 
+		 * al codi actual. 
+		 * 
+		 * Has de crear Excepcions personalitzades per: Revisió de tipus, 
+		 * introducció de plats, revisió de plats de la comanda. 
+		 * El text ha de ser personalitzat. No cal que propaguis l’excepció.
+		 * Rodeja amb un try/catch cada part que has de revisar amb una excepció. 
+		 */
+	}
 
 }
