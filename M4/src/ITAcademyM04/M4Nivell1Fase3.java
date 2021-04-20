c6fb76dd83de2fb06f027d5be0cf8abdf5fe095e
Victor Cuadrat <46792356+victorcuadrat@users.noreply.github.com>
2021-04-20 08:45:56 +0200

    N1 Fase 3)

--- M4/src/ITAcademyM04/M4Nivell1Fase3.java
+++ M4/src/ITAcademyM04/M4Nivell1Fase3.java
@@ -1,9 +1,32 @@
 package ITAcademyM04;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class M4Nivell1Fase3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		HashMap<String, Integer> menuDiccionari = new HashMap<String, Integer>();
+		
+		menuDiccionari.put("Sopa", 8);
+		menuDiccionari.put("Pizza", 11);
+		menuDiccionari.put("Carn", 15);
+		menuDiccionari.put("Gelat", 6);
+		menuDiccionari.put("Paella", 18);
+		menuDiccionari.put("Vi", 28);
+		menuDiccionari.put("Amanida", 9);
+		menuDiccionari.put("Peix", 17);
+		menuDiccionari.put("Marisc", 37);
+		menuDiccionari.put("Llom", 7);
+		
+		List<String> platsDemanats = new ArrayList<String>();
+
+		platsDemanats.add("Pizza");
+		platsDemanats.add("Sopa");
+		platsDemanats.add("Carbonara");
 
 		/**
 		 * Fase 3
@@ -17,6 +40,20 @@ public class M4Nivell1Fase3 {
 		 * compleixen els requeriments de format i extensió per mitjà del control 
 		 * d'excepcions de Java. 
 		 */
+		
+		int sumaTotal = 0;
+		
+		for (String p:platsDemanats) {
+			try {
+				if (menuDiccionari.containsKey(p)) {
+					sumaTotal += menuDiccionari.get(p);
+				} else {
+					System.out.println("No hi ha: " + p);
+				}
+			} catch(Exception e) {
+			      System.out.println("Alguna cosa no ha funcionat.");
+			}
+		}
+	     System.out.println("La suma total és: " + sumaTotal);
 	}
-
 }
